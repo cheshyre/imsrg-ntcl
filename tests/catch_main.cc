7e6b5f51ad87d2b8043a54c47907de8e9320a3ca
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-01-26 16:47:24 +0100

    Add catch test framework.

---
+++ tests/catch_main.cc
@@ -0,0 +1,5 @@
+// Copyright 2022 Matthias Heinz
+// This file exists only to generate an appropriate main function
+// It must be linked against other test files with actual unit tests
+#define CATCH_CONFIG_MAIN
+#include "tests/catch.hpp"
