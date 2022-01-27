93e689ed90cc257ba761d3ca6ece76b8e666a42b
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-01-27 15:01:01 +0100

    Add isospin projection.

---
+++ imsrg/quantum_numbers/isospin_projection.h
@@ -0,0 +1,74 @@
+// Copyright 2022 Matthias Heinz
+#ifndef IMSRG_QUANTUM_NUMBERS_ISOSPIN_PROJECTION_H_
+#define IMSRG_QUANTUM_NUMBERS_ISOSPIN_PROJECTION_H_
+
+#include <utility>
+
+#include "imsrg/quantum_numbers/coupling/m_jj.h"
+
+namespace imsrg {
+
+class IsospinProj {
+ private:
+  int m_tt_;
+
+ public:
+  explicit IsospinProj(int m_tt) : m_tt_(m_tt) {}
+  explicit IsospinProj(M_JJ m_tt) : IsospinProj(m_tt.AsInt()) {}
+
+  static IsospinProj Proton() { return IsospinProj(1); }
+  static IsospinProj Neutron() { return IsospinProj(-1); }
+
+  int AsInt() const { return m_tt_; }
+  M_JJ AsM_JJ() const { return M_JJ(m_tt_); }
+
+  IsospinProj& operator+=(IsospinProj other) {
+    m_tt_ += other.m_tt_;
+    return *this;
+  }
+  IsospinProj& operator-=(IsospinProj other) {
+    m_tt_ -= other.m_tt_;
+    return *this;
+  }
+
+  void swap(IsospinProj& other) noexcept {
+    using std::swap;
+    swap(m_tt_, other.m_tt_);
+  }
+};
+
+inline void swap(IsospinProj& a, IsospinProj& b) noexcept { a.swap(b); }
+inline bool operator<(IsospinProj a, IsospinProj b) {
+  return a.AsInt() < b.AsInt();
+}
+inline bool operator==(IsospinProj a, IsospinProj b) {
+  return a.AsInt() == b.AsInt();
+}
+inline bool operator<=(IsospinProj a, IsospinProj b) {
+  return a.AsInt() <= b.AsInt();
+}
+inline bool operator>(IsospinProj a, IsospinProj b) {
+  return a.AsInt() > b.AsInt();
+}
+inline bool operator!=(IsospinProj a, IsospinProj b) {
+  return a.AsInt() != b.AsInt();
+}
+inline bool operator>=(IsospinProj a, IsospinProj b) {
+  return a.AsInt() >= b.AsInt();
+}
+
+inline IsospinProj operator+(IsospinProj a, IsospinProj b) {
+  IsospinProj c(a);
+  c += b;
+  return c;
+}
+
+inline IsospinProj operator-(IsospinProj a, IsospinProj b) {
+  IsospinProj c(a);
+  c -= b;
+  return c;
+}
+
+}  // namespace imsrg
+
+#endif  // IMSRG_QUANTUM_NUMBERS_ISOSPIN_PROJECTION_H_
