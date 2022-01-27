bf2e7d6c0a4c8bb7cf5c4a63d5939414f8adc332
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-01-27 15:59:32 +0100

    Add orbital angular momentum.

---
+++ imsrg/quantum_numbers/orbital_ang_mom.h
@@ -0,0 +1,56 @@
+// Copyright 2022 Matthias Heinz
+#ifndef IMSRG_QUANTUM_NUMBERS_ORBITAL_ANG_MOM_H_
+#define IMSRG_QUANTUM_NUMBERS_ORBITAL_ANG_MOM_H_
+
+#include <utility>
+
+#include "lib/GSL/include/gsl/assert"
+
+#include "imsrg/quantum_numbers/coupling/jj.h"
+
+namespace imsrg {
+class OrbitalAngMom {
+ private:
+  int l_;
+
+ public:
+  explicit OrbitalAngMom(int l) : l_(l) { Expects(l >= 0); }
+  explicit OrbitalAngMom(JJ ll) : OrbitalAngMom(ll.AsInt() / 2) {
+    Expects(ll >= JJ(0));
+    Expects(ll.AsInt() % 2 == 0);
+  }
+
+  // Default copy, move, and destructor
+
+  int AsInt() const { return l_; }
+  JJ AsJJ() const { return JJ(2 * l_); }
+
+  void swap(OrbitalAngMom& other) noexcept {
+    using std::swap;
+    swap(l_, other.l_);
+  }
+};
+
+inline void swap(OrbitalAngMom& a, OrbitalAngMom& b) noexcept { a.swap(b); }
+inline bool operator<(OrbitalAngMom a, OrbitalAngMom b) {
+  return a.AsInt() < b.AsInt();
+}
+inline bool operator==(OrbitalAngMom a, OrbitalAngMom b) {
+  return a.AsInt() == b.AsInt();
+}
+inline bool operator<=(OrbitalAngMom a, OrbitalAngMom b) {
+  return a.AsInt() <= b.AsInt();
+}
+inline bool operator>(OrbitalAngMom a, OrbitalAngMom b) {
+  return a.AsInt() > b.AsInt();
+}
+inline bool operator!=(OrbitalAngMom a, OrbitalAngMom b) {
+  return a.AsInt() != b.AsInt();
+}
+inline bool operator>=(OrbitalAngMom a, OrbitalAngMom b) {
+  return a.AsInt() >= b.AsInt();
+}
+
+}  // namespace imsrg
+
+#endif  // IMSRG_QUANTUM_NUMBERS_ORBITAL_ANG_MOM_H_
