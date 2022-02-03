ae94eebb3f910f592eecd5f344543dbaeffe222f
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-02-03 12:44:54 +0100

    Add external library abseil.

---
+++ bin/install_abseil.sh
@@ -0,0 +1,16 @@
+# General installation directory
+DEST_DIR=$PWD/lib/build
+# Move to abseil-extracted src directory
+cd lib/src/abseil-cpp/
+# Remove a build directory if it exists
+rm -rf build
+# Make and enter build directory
+mkdir build && cd build
+# Configure cmake to build abseil here and target install directory
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$DEST_DIR ..
+# Build and install
+cmake --build . --target install --config Release
+# Clean up build directory
+cd .. && rm -rf build
+# Create empty file to indicate that abseil was installed
+touch $DEST_DIR/abseil-cpp-installed.txt
