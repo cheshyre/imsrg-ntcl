75b6eebcc6dc26a7b91e034e6b18b2607cad7738
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-02-10 15:28:43 +0100

    Split sp_channel_key into header and src.

--- imsrg/model_space/basis/sp_channel_key.h
+++ imsrg/model_space/basis/sp_channel_key.h
@@ -2,31 +2,14 @@
 #ifndef IMSRG_MODEL_SPACE_BASIS_SP_CHANNEL_KEY_H_
 #define IMSRG_MODEL_SPACE_BASIS_SP_CHANNEL_KEY_H_
 
-#include <cmath>
 #include <utility>
 
-#include "imsrg/assert.h"
 #include "imsrg/model_space/jj_p_m_tt.h"
 #include "imsrg/quantum_numbers/isospin_projection.h"
 #include "imsrg/quantum_numbers/parity.h"
 #include "imsrg/quantum_numbers/total_ang_mom.h"
 
 namespace imsrg {
-
-namespace detail {
-inline std::size_t ComputeSPChannelKeyIndex(TotalAngMom jj, Parity p,
-                                            IsospinProj m_tt) {
-  // m_t should be -1/2 or 1/2 (nucleon)
-  Expects(std::abs(m_tt.AsInt()) == 1);
-
-  // Final 2 lines interpret even parity as 0, odd parity as 2,
-  // neutron as 0, proton as 1
-  // mapping p and m_tt on [0, 3]
-  return 4 * static_cast<std::size_t>(jj.AsInt()) +
-         2 * static_cast<std::size_t>(p.IsOdd()) +
-         1 * static_cast<std::size_t>(m_tt == IsospinProj::Proton());
-}
-}  // namespace detail
 
 class SPChannelKey {
  private:
@@ -36,19 +19,9 @@ class SPChannelKey {
   std::size_t index_;
 
  public:
-  explicit SPChannelKey(TotalAngMom jj, Parity p, IsospinProj m_tt)
-      : jj_(jj),
-        p_(p),
-        m_tt_(m_tt),
-        index_(imsrg::detail::ComputeSPChannelKeyIndex(jj, p, m_tt)) {
-    // j should be half integer
-    Expects(jj_.AsInt() % 2 != 0);
-    // m_t should be -1/2 or 1/2 (nucleon)
-    Expects(std::abs(m_tt_.AsInt()) == 1);
-  }
+  explicit SPChannelKey(TotalAngMom jj, Parity p, IsospinProj m_tt);
 
-  explicit SPChannelKey(JJ_P_M_TT jjpmtt)
-      : SPChannelKey(jjpmtt.JJ(), jjpmtt.P(), jjpmtt.M_TT()) {}
+  explicit SPChannelKey(JJ_P_M_TT jjpmtt);
 
   TotalAngMom JJ() const { return jj_; }
   Parity P() const { return p_; }
@@ -57,13 +30,7 @@ class SPChannelKey {
   // Unique index, computed from jj, p, m_tt
   std::size_t Index() const { return index_; }
 
-  void swap(SPChannelKey& other) noexcept {
-    using std::swap;
-    swap(jj_, other.jj_);
-    swap(p_, other.p_);
-    swap(m_tt_, other.m_tt_);
-    swap(index_, other.index_);
-  }
+  void swap(SPChannelKey& other) noexcept;
 
   // Hash value, independent of secured basis ptr
   template <typename H>
@@ -74,29 +41,17 @@ class SPChannelKey {
 
 inline void swap(SPChannelKey& a, SPChannelKey& b) noexcept { a.swap(b); }
 
-inline bool operator==(const SPChannelKey& a, const SPChannelKey& b) {
-  return a.Index() == b.Index();
-}
+bool operator==(const SPChannelKey& a, const SPChannelKey& b);
 
-inline bool operator!=(const SPChannelKey& a, const SPChannelKey& b) {
-  return a.Index() != b.Index();
-}
+bool operator!=(const SPChannelKey& a, const SPChannelKey& b);
 
-inline bool operator<(const SPChannelKey& a, const SPChannelKey& b) {
-  return a.Index() < b.Index();
-}
+bool operator<(const SPChannelKey& a, const SPChannelKey& b);
 
-inline bool operator<=(const SPChannelKey& a, const SPChannelKey& b) {
-  return a.Index() <= b.Index();
-}
+bool operator<=(const SPChannelKey& a, const SPChannelKey& b);
 
-inline bool operator>(const SPChannelKey& a, const SPChannelKey& b) {
-  return a.Index() > b.Index();
-}
+bool operator>(const SPChannelKey& a, const SPChannelKey& b);
 
-inline bool operator>=(const SPChannelKey& a, const SPChannelKey& b) {
-  return a.Index() >= b.Index();
-}
+bool operator>=(const SPChannelKey& a, const SPChannelKey& b);
 
 }  // namespace imsrg
 
