91ba3be29390880d262a3461277b0121b539b05e
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-02-10 15:17:46 +0100

    "Simplify" SPChannelKey index computation.

--- imsrg/model_space/basis/sp_channel_key.h
+++ imsrg/model_space/basis/sp_channel_key.h
@@ -19,22 +19,12 @@ inline std::size_t ComputeSPChannelKeyIndex(TotalAngMom jj, Parity p,
   // m_t should be -1/2 or 1/2 (nucleon)
   Expects(std::abs(m_tt.AsInt()) == 1);
 
-  std::size_t index = 0;
-  if (m_tt == IsospinProj::Neutron()) {
-    index = 0;
-  } else {  // Proton
-    index = 1;
-  }
-
-  if (p.IsEven()) {
-    index += 0;
-  } else {  // Odd
-    index += 2;
-  }
-
-  index += 4 * static_cast<std::size_t>(jj.AsInt());
-
-  return index;
+  // Final 2 lines interpret even parity as 0, odd parity as 2,
+  // neutron as 0, proton as 1
+  // mapping p and m_tt on [0, 3]
+  return 4 * static_cast<std::size_t>(jj.AsInt()) +
+         2 * static_cast<std::size_t>(p.IsOdd()) +
+         1 * static_cast<std::size_t>(m_tt == IsospinProj::Proton());
 }
 }  // namespace detail
 
