ad56b41b59d1922c29d650bbdac87e2b2649059b
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-02-10 13:54:11 +0100

    Move full_basis factory method to source file.

--- imsrg/model_space/basis/full_basis.h
+++ imsrg/model_space/basis/full_basis.h
@@ -6,11 +6,7 @@
 #include <vector>
 
 #include "imsrg/model_space/basis/states.h"
-#include "imsrg/quantum_numbers/coupling/coupling_ranges.h"
 #include "imsrg/quantum_numbers/ho_energy.h"
-#include "imsrg/quantum_numbers/isospin_projection.h"
-#include "imsrg/quantum_numbers/spin.h"
-#include "imsrg/quantum_numbers/total_ang_mom.h"
 
 namespace imsrg {
 
@@ -21,23 +17,7 @@ class SPFullBasis {
  public:
   explicit SPFullBasis(const std::vector<SPState>& states) : states_(states) {}
 
-  static SPFullBasis FromEMax(HOEnergy emax) {
-    std::vector<SPState> states;
-
-    for (const auto e : GenerateHOEnergyShells(emax)) {
-      for (const auto [n, l] : e.GenerateSubShellQuantumNumbers()) {
-        for (const auto jj :
-             GenerateCouplingRange<TotalAngMom>(l, Spin::OneHalf())) {
-          for (const auto m_tt :
-               {IsospinProj::Neutron(), IsospinProj::Proton()}) {
-            states.push_back(SPState(n, l, jj, m_tt));
-          }
-        }
-      }
-    }
-
-    return SPFullBasis(states);
-  }
+  static SPFullBasis FromEMax(HOEnergy emax);
 
   // TODO(mheinz): implement factory method that includes reference state
   // information
