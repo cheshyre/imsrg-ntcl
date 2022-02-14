2aa13c9fe6b1104c7f5af087916c103f2121c7eb
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-02-14 14:29:45 +0100

    Add some more coupling range tricks.

--- imsrg/quantum_numbers/coupling/coupling_ranges.cc
+++ imsrg/quantum_numbers/coupling/coupling_ranges.cc
@@ -9,12 +9,18 @@
 namespace imsrg {
 
 std::vector<imsrg::JJ> GenerateCouplingRange(JJ a, JJ b) {
-  imsrg::JJ min_jj(std::abs(a.AsInt() - b.AsInt()));
-  imsrg::JJ max_jj(a.AsInt() + b.AsInt());
+  const auto min_jj = CouplingMinimum(a, b);
+  const auto max_jj = CouplingMaximum(a, b);
 
+  return CouplingRangeFromMinAndMax(min_jj, max_jj);
+}
+
+JJ CouplingMinimum(JJ a, JJ b) { return JJ(std::abs(a.AsInt() - b.AsInt())); }
+JJ CouplingMaximum(JJ a, JJ b) { return JJ(a.AsInt() + b.AsInt()); }
+std::vector<imsrg::JJ> CouplingRangeFromMinAndMax(JJ min, JJ max) {
   std::vector<imsrg::JJ> jjs;
-  jjs.reserve((max_jj.AsInt() - min_jj.AsInt()) / 2 + 1);
-  for (int jj_i = min_jj.AsInt(); jj_i <= max_jj.AsInt(); jj_i += 2) {
+  jjs.reserve((max.AsInt() - min.AsInt()) / 2 + 1);
+  for (int jj_i = min.AsInt(); jj_i <= max.AsInt(); jj_i += 2) {
     jjs.push_back(imsrg::JJ(jj_i));
   }
 
