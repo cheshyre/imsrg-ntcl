a0c89d6970e1860760881b0c94c95ce2e59e661a
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-02-28 18:30:34 +0100

    Add functioning comm_121 implementation.

---
+++ imsrg/commutator/scalar/comm_121.cc
@@ -0,0 +1,136 @@
+// Copyright 2022 Matthias Heinz
+#include "imsrg/commutator/scalar/comm_121.h"
+
+#include "imsrg/model_space/scalar/one_body/channel_key.h"
+#include "imsrg/operator/scalar/one_body/operator.h"
+#include "imsrg/operator/scalar/two_body/operator.h"
+#include "imsrg/quantum_numbers/coupling/factors.h"
+
+namespace imsrg {
+
+namespace detail {
+
+static void EvaluateScalar121CommutatorWithFactor(const Scalar1BOperator& a,
+                                                  const Scalar2BOperator& b,
+                                                  Scalar1BOperator& c,
+                                                  int factor);
+
+static void EvaluateScalar121CommutatorRefImplWithFactor(
+    const Scalar1BOperator& a, const Scalar2BOperator& b, Scalar1BOperator& c,
+    int factor);
+
+}  // namespace detail
+
+void EvaluateScalar121Commutator(const Scalar1BOperator& a,
+                                 const Scalar2BOperator& b,
+                                 Scalar1BOperator& c) {
+  imsrg::detail::EvaluateScalar121CommutatorWithFactor(a, b, c, 1);
+}
+
+void EvaluateScalar121CommutatorRefImpl(const Scalar1BOperator& a,
+                                        const Scalar2BOperator& b,
+                                        Scalar1BOperator& c) {
+  imsrg::detail::EvaluateScalar121CommutatorRefImplWithFactor(a, b, c, 1);
+}
+
+// Evaluates scalar [2, 1] -> 1 commutator.
+void EvaluateScalar211Commutator(const Scalar2BOperator& a,
+                                 const Scalar1BOperator& b,
+                                 Scalar1BOperator& c) {
+  imsrg::detail::EvaluateScalar121CommutatorWithFactor(b, a, c, -1);
+}
+
+// Evaluates scalar [2, 1] -> 1 commutator.
+void EvaluateScalar211CommutatorRefImpl(const Scalar2BOperator& a,
+                                        const Scalar1BOperator& b,
+                                        Scalar1BOperator& c) {
+  imsrg::detail::EvaluateScalar121CommutatorRefImplWithFactor(b, a, c, -1);
+}
+
+namespace detail {
+
+void EvaluateScalar121CommutatorWithFactor(const Scalar1BOperator& a,
+                                           const Scalar2BOperator& b,
+                                           Scalar1BOperator& c, int factor) {
+  return EvaluateScalar121CommutatorRefImplWithFactor(a, b, c, factor);
+}
+
+static void EvaluateScalar121CommutatorRefImplWithFactor(
+    const Scalar1BOperator& a, const Scalar2BOperator& b, Scalar1BOperator& c,
+    int factor) {
+  Expects(a.GetModelSpacePtr() == c.GetModelSpacePtr());
+  Expects(c.Herm() == Hermiticity::CommutatorHermiticity(a.Herm(), b.Herm()));
+
+  const auto& ms_1b = a.GetModelSpace();
+  const auto& ms_2b = b.GetModelSpace();
+
+  for (std::size_t chan_b_index = 0; chan_b_index < ms_2b.NumberOfChannels();
+       chan_b_index += 1) {
+    const auto& chan_b = ms_2b.ChannelAtIndex(chan_b_index);
+    const auto& chan_1 = chan_b.BraChannel1();
+    const auto& chan_q = chan_b.BraChannel2();
+    const auto& chan_2 = chan_b.KetChannel1();
+    const auto& chan_p = chan_b.KetChannel2();
+
+    if (chan_1.ChannelKey() != chan_2.ChannelKey()) {
+      continue;
+    }
+
+    if (chan_p.ChannelKey() != chan_q.ChannelKey()) {
+      continue;
+    }
+
+    // Derived channels
+    const imsrg::Scalar1BChannelKey chankey_a(chan_p.ChannelKey(),
+                                              chan_q.ChannelKey());
+    if (!ms_1b.IsChannelInModelSpace(chankey_a)) {
+      continue;
+    }
+    const auto chan_a_index = ms_1b.IndexOfChannelInModelSpace(chankey_a);
+
+    const imsrg::Scalar1BChannelKey chankey_c(chan_1.ChannelKey(),
+                                              chan_2.ChannelKey());
+    if (!ms_1b.IsChannelInModelSpace(chankey_c)) {
+      continue;
+    }
+    const auto chan_c_index = ms_1b.IndexOfChannelInModelSpace(chankey_c);
+
+    double channel_factor =
+        (1.0 * factor *
+         imsrg::HatSquared(chan_b.ChannelKey().OpChannel().JJ())) /
+        imsrg::HatSquared(chankey_c.OpChannel().JJ());
+
+    // Tensors
+    auto& tensor_c = c.GetMutableTensorAtIndex(chan_c_index);
+    const auto& tensor_a = a.GetTensorAtIndex(chan_a_index);
+    const auto& tensor_b = b.GetTensorAtIndex(chan_b_index);
+
+    // Dims
+    const auto dim_p = chan_p.size();
+    const auto dim_q = chan_q.size();
+    const auto dim_1 = chan_1.size();
+    const auto dim_2 = chan_2.size();
+
+    // Occs
+    const auto& occs_p = chan_p.ChannelBasis().Occs();
+    const auto& occsbar_p = chan_p.ChannelBasis().OccsBar();
+    const auto& occs_q = chan_q.ChannelBasis().Occs();
+    const auto& occsbar_q = chan_q.ChannelBasis().OccsBar();
+
+    for (std::size_t i2 = 0; i2 < dim_2; i2 += 1) {
+      for (std::size_t i1 = 0; i1 < dim_1; i1 += 1) {
+        for (std::size_t q = 0; q < dim_q; q += 1) {
+          for (std::size_t p = 0; p < dim_p; p += 1) {
+            tensor_c(i1, i2) += channel_factor * occs_p[p] * occsbar_q[q] *
+                                tensor_a(p, q) * tensor_b(i1, q, i2, p);
+            tensor_c(i1, i2) -= channel_factor * occsbar_p[p] * occs_q[q] *
+                                tensor_a(p, q) * tensor_b(i1, q, i2, p);
+          }
+        }
+      }
+    }
+  }
+}
+
+}  // namespace detail
+}  // namespace imsrg
