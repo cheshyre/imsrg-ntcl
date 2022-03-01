b33920db2d4d70db5f7054a735e1cd557a80bfdf
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-03-01 13:50:24 +0100

    Add 2B model space lookup.

--- imsrg/model_space/scalar/two_body/model_space.cc
+++ imsrg/model_space/scalar/two_body/model_space.cc
@@ -6,12 +6,38 @@
 #include <utility>
 #include <vector>
 
+#include "absl/container/flat_hash_map.h"
+
+#include "imsrg/assert.h"
 #include "imsrg/model_space/scalar/two_body/channel.h"
+#include "imsrg/model_space/scalar/two_body/channel_key.h"
 #include "imsrg/model_space/single_particle/model_space.h"
 #include "imsrg/quantum_numbers/coupling/coupling_ranges.h"
 #include "imsrg/quantum_numbers/total_ang_mom.h"
 
 namespace imsrg {
+
+namespace detail {
+static absl::flat_hash_map<imsrg::Scalar2BChannelKey, std::size_t>
+GenerateChannelIndexLookup(const std::vector<imsrg::Scalar2BChannel>& chans);
+}
+
+bool Scalar2BModelSpace::IsChannelInModelSpace(
+    Scalar2BChannelKey chankey) const {
+  const auto search = chan_index_lookup_.find(chankey);
+  return search != chan_index_lookup_.end();
+}
+
+std::size_t Scalar2BModelSpace::IndexOfChannelInModelSpace(
+    Scalar2BChannelKey chankey) const {
+  // Expects(IsChannelInModelSpace(chankey));
+  // ^ We check this later
+  const auto search = chan_index_lookup_.find(chankey);
+  Expects(search != chan_index_lookup_.end());
+
+  return search->second;
+}
+
 std::shared_ptr<const Scalar2BModelSpace> Scalar2BModelSpace::FromSPModelSpace(
     const SPModelSpace& sp_ms) {
   const auto& sp_chans = sp_ms.Channels();
@@ -66,5 +92,21 @@ std::shared_ptr<const Scalar2BModelSpace> Scalar2BModelSpace::FromSPModelSpace(
 }
 
 Scalar2BModelSpace::Scalar2BModelSpace(std::vector<Scalar2BChannel>&& chans)
-    : chans_(std::move(chans)) {}
+    : chans_(std::move(chans)),
+      chan_index_lookup_(imsrg::detail::GenerateChannelIndexLookup(chans_)) {}
+
+namespace detail {
+
+absl::flat_hash_map<imsrg::Scalar2BChannelKey, std::size_t>
+GenerateChannelIndexLookup(const std::vector<imsrg::Scalar2BChannel>& chans) {
+  absl::flat_hash_map<imsrg::Scalar2BChannelKey, std::size_t> index_lookup;
+  index_lookup.reserve(chans.size());
+
+  for (std::size_t index = 0; index < chans.size(); index += 1) {
+    index_lookup[chans[index].ChannelKey()] = index;
+  }
+  return index_lookup;
+}
+}  // namespace detail
+
 }  // namespace imsrg
